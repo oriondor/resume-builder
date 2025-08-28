1b6cf90dc67a52436b0fcbe3408a86dc82102fda
Vlat <vladislav.pikalov@gmail.com>
2025-08-28 19:38:39 +0200

    component improvements

--- components/HelperComponents/Button.vue
+++ components/HelperComponents/Button.vue
@@ -2,11 +2,15 @@
   interface Props {
     type?: "primary" | "secondary" | "subdued";
     icon?: string;
+    loading?: boolean;
+    disabled?: boolean;
   }
 
   const props = withDefaults(defineProps<Props>(), {
     type: "primary",
   });
+
+  const { loading, disabled } = toRefs(props);
 
   const attrs = useAttrs();
   const slots = useSlots();
@@ -17,9 +21,14 @@
     return hasIcon && !hasDefault;
   });
 
-  defineEmits<{
+  const emit = defineEmits<{
     (e: "click"): void;
   }>();
+
+  function click() {
+    if (loading.value || disabled.value) return;
+    emit("click");
+  }
 </script>
 
 <template>
@@ -27,12 +36,17 @@
     <button
       v-bind="attrs"
       :class="[type, 'gradient-hover', { 'icon-only': isIconOnly }]"
-      @click="$emit('click')"
+      @click="click"
     >
       <slot name="icon">
         <Icon v-if="icon" :name="icon" />
       </slot>
-      <slot />
+      <template v-if="loading">
+        <helper-loading-spinner />
+      </template>
+      <template v-else>
+        <slot />
+      </template>
     </button>
   </helper-control-element>
 </template>
@@ -47,6 +61,7 @@
     cursor: pointer;
     display: inline-flex;
     align-items: center;
+    gap: 0.5rem;
 
     &.icon-only {
       padding: 0;
---
+++ components/HelperComponents/LoadingSpinner.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+  <Icon name="line-md:loading-loop" />
+</template>
